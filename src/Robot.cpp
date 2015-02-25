9784d3472cbe62cc28456f0736e307033cee8318
Nathan Johnson <nathancj224@gmail.com>
2015-02-25 18:34:35 -0500

    change auton mode. add two tote auton method.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -8,7 +8,7 @@
  * 1 = DriveForwardAuton
  * 2 = OneToteAuton
  */
-#define AUTON_MODE 2
+#define AUTON_MODE 1
 
 class Robot: public SampleRobot
 {
@@ -59,7 +59,7 @@ public:
 	 */
 	void AutonInit() {
 		autonStage = 0;
-		SmartDashboard::PutNumber("Autonomous Mode", AUTON_MODE);
+		//SmartDashboard::PutNumber("Autonomous Mode", AUTON_MODE);
 	}
 
 	/**
@@ -75,6 +75,9 @@ public:
 			break;
 			case 2:
 				OneToteAuton();
+			break;
+			case 3:
+				TwoToteAuton();
 			break;
 		}
 	}
@@ -131,6 +134,7 @@ public:
 	}
 
 	/**
+	 * OneToteAuton
 	 * Picks up first auton (yellow) tote and brings it into the auto zone
 	 */
 	void OneToteAuton()
@@ -153,6 +157,81 @@ public:
 				}
 			break;
 			// back up
+			case 2:
+				if(time < 0.5) {
+					robotDrive.TankDrive(-0.5, -0.5);
+				}
+				else {
+					RestartAutonTimer();
+					autonStage = 3;
+				}
+			break;
+			// turn 90 right
+			case 3:
+				if(time < 1.01) {
+					robotDrive.TankDrive(0.57, -0.57);
+				}
+				else {
+					RestartAutonTimer();
+					autonStage = 4;
+				}
+			break;
+			// drive forward
+			case 4:
+				if(time < 5) {
+					robotDrive.TankDrive(0.65, 0.65);
+				}
+				else {
+					RestartAutonTimer();
+					autonStage = 5;
+				}
+			break;
+			// turn 90 left
+			case 5:
+				if(time < 1.4) {
+					robotDrive.TankDrive(0.5, -0.5);
+				}
+				else {
+					RestartAutonTimer();
+					autonStage = 6;
+				}
+			break;
+			// lower arm
+			case 6:
+				ArmDown();
+				RestartAutonTimer();
+				autonStage = 7;
+			break;
+		}
+
+	}
+
+	/**
+	 * TwoToteAuton
+	 * Picks up first auton tote, goes around bin, lowers tote onto
+	 * second auton tote, then it picks them up and and brings them
+	 * into the auto zone.
+	 */
+	void TwoToteAuton()
+	{
+		int time = autonTimer.Get();
+		switch(autonStage) {
+			case 0:
+				ArmDown();
+				RestartAutonTimer();
+				autonStage = 1;
+			break;
+			// lift arm
+			case 1:
+				if(time < 0.5) {
+					ArmUp();
+				}
+				else {
+					RestartAutonTimer();
+					autonStage = 2;
+				}
+			break;
+			// turn right
 			case 2:
 				if(time < 0.5) {
 					robotDrive.TankDrive(-0.5, -0.5);
